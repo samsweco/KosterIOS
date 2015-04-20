dfacd46357433341d8e1a806317c75ad1a92142e
supersandra <svictorin@live.se>
2015-04-20 16:17:23 +0200

    äventyrsled på interactive

--- app/controllers/interactive.js
+++ app/controllers/interactive.js
@@ -1,5 +1,8 @@
 var args = arguments[0] || {};
+
 showMap();
+createMapRoutes();
+var familyMap;
 
 //-----------------------------------------------------------
 // Öppnar vy och läser in nästa fråga
@@ -139,3 +142,91 @@ function showMap() {
 		 newError("Något gick fel när sidan skulle laddas, prova igen!", "Map - showMap");
 	 }
 }
+
+//-----------------------------------------------------------
+// Beräknar nivån av inzoomning på en vald led
+//-----------------------------------------------------------
+function calculateMapRegion(trailCoordinates) {
+	try {
+		if (trailCoordinates.length != 0) {
+			var poiCenter = {};
+			var delta = 0.02;
+			var minLat = trailCoordinates[0].latitude,
+			    maxLat = trailCoordinates[0].latitude,
+			    minLon = trailCoordinates[0].longitude,
+			    maxLon = trailCoordinates[0].longitude;
+			for (var i = 0; i < trailCoordinates.length - 1; i++) {
+				minLat = Math.min(trailCoordinates[i + 1].latitude, minLat);
+				maxLat = Math.max(trailCoordinates[i + 1].latitude, maxLat);
+				minLon = Math.min(trailCoordinates[i + 1].longitude, minLon);
+				maxLon = Math.max(trailCoordinates[i + 1].longitude, maxLon);
+			}
+
+			var deltaLat = maxLat - minLat;
+			var deltaLon = maxLon - minLon;
+
+			delta = Math.max(deltaLat, deltaLon);
+			//Change multiplier if it's too close
+			delta = delta * 1.4;
+
+			poiCenter.lat = maxLat - parseFloat((maxLat - minLat) / 2);
+			poiCenter.lon = maxLon - parseFloat((maxLon - minLon) / 2);
+
+			region = {
+				latitude : poiCenter.lat,
+				longitude : poiCenter.lon,
+				latitudeDelta : delta,
+				longitudeDelta : delta
+				
+			};
+		}
+		return region;
+		
+	} catch(e) {
+		newError("Något gick fel när sidan skulle laddas, prova igen!", "MapDetail - calculateMapRegion");
+	}
+
+}
+
+//-----------------------------------------------------------
+// skapar vandringsleden och sätter den på kartan
+//-----------------------------------------------------------
+function createMapRoutes() {
+	try {
+		var zoomedRoute = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory + "/routes/adventureroute.json").read().text;
+		var parsedRoute = JSON.parse(zoomedRoute);
+
+		var geoArray = [];
+		geoArray.push(parsedRoute);
+
+		var coordArray = [];
+
+		for (var u = 0; u < geoArray.length; u++) {
+			var coords = geoArray[0].features[0].geometry.paths[u];
+
+			for (var i = 0; i < coords.length; i++) {
+
+				var point = {
+					latitude : coords[i][1],
+					longitude : coords[i][0]
+				};
+				coordArray.push(point);
+			}
+
+			var route = {
+				name : 'Äventyrsleden',
+				points : coordArray,
+				color : 'purple',
+				width : 2.0
+			};
+			
+			familyMap.addRoute(MapModule.createRoute(route));
+		}
+
+		familyMap.region = calculateMapRegion(coordArray);
+	} catch(e) {
+		newError("Något gick fel när sidan skulle laddas, prova igen!", "Map - createMapRoute");
+	}
+}
+
+
