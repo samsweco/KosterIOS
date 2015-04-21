0d712578021b58e41ade696047323e4851078186
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-04-21 08:17:08 +0200

    mer widget stuff

--- app/alloy.js
+++ app/alloy.js
@@ -124,3 +124,31 @@ function showInteractive() {
 		interactiveVisible = false;
 	}
 }
+
+var letters = Alloy.Collections.letterModel;
+letters.fetch({query : 'select * from letterModel'});
+
+	
+function sendLetter() {
+	checkLetter(getLetter());
+}
+
+function getLetter() {
+	var letter = $.txtLetter.value;
+	alert(letter);
+	return letter.toUpperCase();
+}
+
+function checkLetter(letterToCheck) {
+	
+	var letterJSON = letters.toJSON();
+	
+	for (var i = 0; i < letterJSON.length; i++) {
+		if (letterJSON[i].letter == letterToCheck) {
+			//Save letter
+			lettersArray.push(letterJSON[i].letter);
+			$.lblLetters.text += letterJSON[i].letter;
+			//alert(letterJSON[i].letter);
+		}
+	}
+}
