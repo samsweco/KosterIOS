ce35e3646e0b42af466f9ca224ed01d818251604
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-05-08 16:04:01 +0200

    infolist

--- app/controllers/infoDetail.js
+++ app/controllers/infoDetail.js
@@ -1,52 +1,114 @@
 var args = arguments[0] || {};
+var urlCollection = Alloy.Collections.info_urlModel;
 
 //-----------------------------------------------------------
 // Args skickade från listvy
 //-----------------------------------------------------------
 try {
 	$.lblInfoTitle.text = args.name || "Title";
-	$.lblInfoText.text = args.infoTxt || "Info";
-	$.lblInfoLink.text = args.desc || "url";
 	$.infoImg.image = "/pics/" + args.img;
 	var id = args.id;
-	
-	var infoid = args.id;
 
-	// var link = $.lblInfoLink;
-	// link.addEventListener('click', function(e) {
-		// openLink(url);
-	// });
-	
 } catch(e) {
 	newError("Något gick fel när sidan skulle laddas, prova igen!", "infoDetail - load data into labels");
 }
 
-function createLinks(){
-	var urlCollection = Alloy.Collections.info_urlModel;
-	urlCollection.fetch({
-		query : 'SELECT * from info_urlModel WHERE info_id ="' + id + '"'
-	});
-	
-	var urlJson = urlCollection.toJSON();
-	
-	for(var i = 0; i<urlJson.length; i++){
-		
-		var txt = Ti.UI.createLabel({
-			text : urlJson[i].url_text,
-		});
-		
-		var linkName = Ti.UI.createLabel({
-			text : urlJson[i].linkname
-		});
-		
-		 linkName.addEventListener('click', function(e) {
-			openLink(urlJson[i].url);
-			});
-		
-		
-		$.linkView.add(txt);
-		$.linkView.add(linkName);
+setRowData();
+
+//-----------------------------------------------------------
+// Visar info för valt item i listvyn
+//-----------------------------------------------------------
+function showinfoDetails(info) {
+	try {
+		var selectedInfo = info.row;
+		var args = {
+			id : selectedInfo.id,
+			name : selectedInfo.name,
+			infoTxt : selectedInfo.infoTxt,
+			link : selectedInfo.link,
+			img : selectedInfo.image,
+			desc : selectedInfo.desc
+		};
+
+		var infoDetail = Alloy.createController("infoDetail", args).getView();
+		infoDetail.open();
+	} catch(e) {
+		newError("Något gick fel när sidan skulle laddas, prova igen!", "infoDetail - showInfoDetail");
 	}
+
+}
+
+//-----------------------------------------------------------
+// sätter alla items i listan
+//-----------------------------------------------------------
+function setRowData() {
+	try {
+		var tableViewData = [];
+		urlCollection.fetch({
+			query : 'SELECT * from info_urlModel WHERE info_id ="' + id + '"'
+		});
+
+		var urlJson = urlCollection.toJSON();
+
+		for (var i = 0; i < urlJson.length; i++) {
+			var row = Ti.UI.createTableViewRow({
+				id : urlJson[i].id,
+				layout : 'horizontal',
+				height : '60dp',
+				top : '0dp'
+			});
+
+			var txt = Ti.UI.createLabel({
+				left : '15dp',
+				right : '15dp',
+				width : Ti.UI.FILL,
+				font : {
+					fontSize : '12dp',
+					fontStyle : 'italic'
+				},
+				text : urlJson[i].url_text,
+			});
+
+			var linkName = Ti.UI.createLabel({
+				width : Ti.UI.FILL,
+				left : '15dp',
+				right : '15dp',
+				font : {
+					fontSize : '12dp',
+				},
+				color : 'blue',
+				text : urlJson[i].linkname
+			});
+			row.add(txt);
+			row.add(linkName);
+
+			tableViewData.push(row);
+		}
+		$.tableView.data = tableViewData;
+
+	} catch(e) {
+		newError("Något gick fel när sidan skulle laddas, prova igen!", "infoList - setRowData");
+	}
+}
+
+//-----------------------------------------------------------
+// Hämtar all info som ska läsas in i listan
+//-----------------------------------------------------------
+function getLink(e) {
+
+		var rowId = e.rowData.id;
+		urlCollection.fetch({
+			query : 'SELECT url FROM info_urlModel WHERE id = "' + rowId + '"'
+		});
+
+		var jsonObj = urlCollection.toJSON();
+
+		var web =  jsonObj[0].url;
+	
+	openLink(web);
+
+
+
 }
 
 //-----------------------------------------------------------
@@ -59,10 +121,10 @@ function openLink(link) {
 		});
 		var window = Titanium.UI.createWindow();
 		window.backButtonTitle = "Tillbaka";
-		
+
 		window.add(webview);
 		Alloy.CFG.tabs.activeTab.open(window);
-		
+
 	} catch(e) {
 		newError("Något gick fel när sidan skulle laddas, prova igen!", "infoDetail - openLink");
 	}
