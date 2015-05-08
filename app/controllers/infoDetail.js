3543422bb4e8013b1f99347c9d697950c47a3ab8
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-05-08 14:46:01 +0200

    interactive stuff

--- app/controllers/infoDetail.js
+++ app/controllers/infoDetail.js
@@ -8,17 +8,45 @@ try {
 	$.lblInfoText.text = args.infoTxt || "Info";
 	$.lblInfoLink.text = args.desc || "url";
 	$.infoImg.image = "/pics/" + args.img;
+	var id = args.id;
 	
 	var infoid = args.id;
-	var url = args.link;
 
-	var link = $.lblInfoLink;
-	link.addEventListener('click', function(e) {
-		openLink(url);
-	});
+	// var link = $.lblInfoLink;
+	// link.addEventListener('click', function(e) {
+		// openLink(url);
+	// });
 	
 } catch(e) {
 	newError("Något gick fel när sidan skulle laddas, prova igen!", "infoDetail - load data into labels");
+}
+
+function createLinks(){
+	var urlCollection = Alloy.Collections.info_urlModel;
+	urlCollection.fetch({
+		query : 'SELECT * from info_urlModel WHERE info_id ="' + id + '"'
+	});
+	
+	var urlJson = urlCollection.toJSON();
+	
+	for(var i = 0; i<urlJson.length; i++){
+		
+		var txt = Ti.UI.createLabel({
+			text : urlJson[i].url_text,
+		});
+		
+		var linkName = Ti.UI.createLabel({
+			text : urlJson[i].linkname
+		});
+		
+		 linkName.addEventListener('click', function(e) {
+			openLink(urlJson[i].url);
+			});
+		
+		
+		$.linkView.add(txt);
+		$.linkView.add(linkName);
+	}
 }
 
 //-----------------------------------------------------------
