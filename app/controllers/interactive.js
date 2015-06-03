fd9b85cdb3f743c7a3ce4380ad18121ebe1fae42
supersandra <svictorin@live.se>
2015-06-03 14:45:37 +0200

    vbn

--- app/controllers/interactive.js
+++ app/controllers/interactive.js
@@ -29,7 +29,11 @@ function displayMap() {
 		$.showFamilyTrail.add(showDetailMap(interactiveMap, 7, 'Äventyrsleden', 'purple'));
 		addClueZone();
 		displaySpecificMarkers(7, interactiveMap);
-		interactiveMap.addEventListener('click', evtList);
+		interactiveMap.addEventListener('click', function(evt){
+			if (evt.clicksource == 'rightButton') {
+				showHotspot(evt.annotation.id);
+			}
+		});
 	} catch(e) {
 		newError("Något gick fel när sidan skulle laddas, prova igen!", "Bokstavsjakten");
 	}
@@ -83,7 +87,7 @@ function startInteractive() {
 // Laddar in nästa ledtråd om man inte hittar bokstaven
 //-----------------------------------------------------------
 function toNextClue() {
-	try {
+	// try {
 		var nextDialog = Ti.UI.createAlertDialog({
 			title : 'Gå till nästa',
 			message : 'Är du säker på att du inte hittar bokstaven?',
@@ -112,25 +116,25 @@ function toNextClue() {
 		});
 
 		nextDialog.show();
-	} catch(e) {
-		newError("Något gick fel när sidan skulle laddas, prova igen!", "Bokstavsjakten");
-	}
+	// } catch(e) {
+		// newError("Något gick fel när sidan skulle laddas, prova igen!", "Bokstavsjakten");
+	// }
 }
 
 //-----------------------------------------------------------
 // Laddar in första ledtråden
 //-----------------------------------------------------------
 function loadClue(id) {
-	try {
+	// try {
 		if (id < 10){
 			$.lblWelcome.text = "Ledtråd " + id + ":";
 			$.lblInfoText.text = Alloy.Globals.jsonCollection[id - 1].clue; 
 		} else {
 			allLetters();
 		}
-	} catch(e) {
-		newError("Något gick fel när sidan skulle laddas, prova igen!", "Bokstavsjakten");
-	}
+	// } catch(e) {
+		// newError("Något gick fel när sidan skulle laddas, prova igen!", "Bokstavsjakten");
+	// }
 }
 
 Alloy.Globals.loadClue = loadClue;
