77ae15525c2581c32e6b19860fa43583a6dcc8c6
supersandra <svictorin@live.se>
2015-06-03 08:27:12 +0200

    index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -156,7 +156,7 @@ function openBoat(){
 function showInfo(){
 	if(!infoVisible){
 		$.widgetInfo.show();
-		$.widgetInfo.height = '80%';
+		$.widgetInfo.height = '75%';
 		infoVisible = true;
 	} else {
 		$.widgetInfo.hide();
