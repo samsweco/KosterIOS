2911ff86a417eaf86e94eba72fd3242df11d24e4
Sandra <sandra.victorin@sweco.se>
2016-05-19 13:33:20 +0200

    Newsfeed

--- app/controllers/newsfeed.js
+++ app/controllers/newsfeed.js
@@ -1,21 +1,26 @@
 //Ti.include('instafeed.html');
 
-var webview = Titanium.UI.createWebView({
-	//html : '<iframe src="" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:230px; height: 575px" ></iframe>'
-	// html: '<!DOCTYPE html><html><head><title>Kosterhavets instagram</title></head>'
-		// +	'<body><!-- INSTANSIVE WIDGET --><script src="//instansive.com/widget/js/instansive.js"></script>'
-		// +	'<iframe src="//instansive.com/widgets/787f0ec8a0f6a0a6433decfea86d5c9c86805ac3.html" id="instansive_787f0ec8a0" name="instansive_787f0ec8a0" scrolling="no" allowtransparency="yes" class="instansive-widget">'
-		// +	'</iframe></body></html>'
-		
-	//url : "/lib/instafeed.html" 
-	url: 'https://www.instagram.com/kosterhavets.nationalpark/'
-});
+openFeedWindow();
 
-$.newsfeedView.add(webview);
-
-//-----------------------------------------------------------
-// Funktion för att stänga och rensa sida när man stänger sidan
-//-----------------------------------------------------------
 function closeFeedWindow(){
 	$.newsfeedWindow.close();
+}
+
+function openFeedWindow(){
+	try {
+		var webview = Titanium.UI.createWebView({
+			// html : '<iframe src="" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:230px; height: 575px" ></iframe>'
+			// html: '<!DOCTYPE html><html><head><title>Kosterhavets instagram</title></head>'
+				// +	'<body><!-- INSTANSIVE WIDGET --><script src="//instansive.com/widget/js/instansive.js"></script>'
+				// +	'<iframe src="//instansive.com/widgets/787f0ec8a0f6a0a6433decfea86d5c9c86805ac3.html" id="instansive_787f0ec8a0" name="instansive_787f0ec8a0" scrolling="no" allowtransparency="yes" class="instansive-widget">'
+				// +	'</iframe></body></html>'
+				
+			//url : "/lib/instafeed.html" 
+			url: 'https://www.instagram.com/kosterhavets.nationalpark/'
+		});
+		
+		$.newsfeedView.add(webview);
+	}  catch(e) {
+		newError("Något gick fel när sidan skulle laddas. Kontrollera din internetanslutning och prova igen!");
+	}
 }
